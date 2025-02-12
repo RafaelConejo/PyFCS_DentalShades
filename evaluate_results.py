af23dca377e0015e3fe833cfe8a184d770000d82
Rafael Vázquez Conejo <72466566+RafaelConejo@users.noreply.github.com>
2025-02-12 17:03:01 +0100

    Evaluate function

---
+++ evaluate_results.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+current_dir = os.path.dirname(__file__)
+sys.path.append(current_dir)
+
+
+############################################################ FUNTIONS ############################################################
+
